b156c36c022970c9ae9d4c77ee05b8dea1190cf2
Anton-beep <anton.plyu@gmail.com>
2024-11-02 15:22:38 +0100

    some mappings fixes and mason

--- init.lua
+++ init.lua
@@ -1,10 +1,14 @@
--- cofig files 
+-- cofig files
 require("config.lazy")
 require("config.colorscheme")
 
--- mason 
+-- mason
 require("mason").setup()
-require("mason-lspconfig").setup()
+require("mason-lspconfig").setup {
+	ensure_installed = {
+		"lua_ls", "harper_ls", "golangci_lint_ls"
+	}
+}
 
 -- nvim-tree
 vim.g.loaded_netrw = 1
@@ -13,23 +17,23 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 require("nvim-tree").setup({
-  sort = {
-    sorter = "case_sensitive",
-  },
-  view = {
-    width = 40,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+	sort = {
+		sorter = "case_sensitive",
+	},
+	view = {
+		width = 40,
+	},
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = true,
+	},
 })
 
--- autocomplete 
+-- autocomplete
 vim.g.coq_settings = {
-    keymap =  { jump_to_mark = "<c-\\" },
+	keymap = { jump_to_mark = "<c-\\" },
 }
 
 -- lsp
@@ -38,6 +42,7 @@ local lspconfig = require('lspconfig')
 -- mappings
 require("mappings")
 
--- relative numbers 
+-- other
 vim.wo.relativenumber = true
-
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
