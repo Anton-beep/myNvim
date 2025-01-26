eda76fbee3ecb4257b4bcd95e75b37ed17d790aa
Anton-beep <anton.plyu@gmail.com>
2025-01-26 17:35:45 +0100

    close buffer map

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,14 +2,14 @@
 
 -- Shorten function name for convenience
 local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true, }  -- Common options
+local opts = { noremap = true, silent = true, } -- Common options
 
 -- Normal mode mappings
-map("n", "<C-s>", ":w<CR>", opts)  -- Save file
-map("n", "<C-h>", "<C-w>h", opts)  -- Move left between splits
-map("n", "<C-l>", "<C-w>l", opts)  -- Move right between splits
-map("n", "<C-j>", "<C-w>j", opts)  -- Move down between splits
-map("n", "<C-k>", "<C-w>k", opts)  -- Move up between splits
+map("n", "<C-s>", ":w<CR>", opts) -- Save file
+map("n", "<C-h>", "<C-w>h", opts) -- Move left between splits
+map("n", "<C-l>", "<C-w>l", opts) -- Move right between splits
+map("n", "<C-j>", "<C-w>j", opts) -- Move down between splits
+map("n", "<C-k>", "<C-w>k", opts) -- Move up between splits
 
 -- Telescope
 local builtin = require('telescope.builtin')
@@ -22,10 +22,10 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help ta
 map("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = false, silent = true })
 
 -- Insert mode mappings
-map("i", "jk", "<ESC>", opts)  -- Fast exit from insert mode
+map("i", "jk", "<ESC>", opts) -- Fast exit from insert mode
 
 -- Visual mode mappings
-map("v", "<", "<gv", opts)  -- Stay in visual mode when indenting
+map("v", "<", "<gv", opts) -- Stay in visual mode when indenting
 map("v", ">", ">gv", opts)
 
 -- map("n", "<leader>bt", ":set bg=light", opts)
@@ -38,39 +38,41 @@ map("n", "<leader>tt", ":tab term<CR>", { desc = "Open terminal in a new tab" })
 
 -- Define the on_attach function to map LSP-related keys
 local on_attach = function(_, bufnr)
-  -- Key mappings, which apply only when an LSP server is attached to the buffer
-  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	-- Key mappings, which apply only when an LSP server is attached to the buffer
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
 
-  vim.keymap.set('n', '<leader>lgd', vim.lsp.buf.definition, { desc = "Go to Definition", unpack(bufopts) })
-  vim.keymap.set('n', '<leader>lgD', vim.lsp.buf.declaration, { desc = "Go to Declaration", unpack(bufopts) })
-  vim.keymap.set('n', '<leader>lgi', vim.lsp.buf.implementation, { desc = "Go to Implementation", unpack(bufopts) })
-  vim.keymap.set('n', '<leader>lgt', vim.lsp.buf.type_definition, { desc = "Go to Type Definition", unpack(bufopts) })
-  vim.keymap.set('n', '<leader>lK', vim.lsp.buf.hover, { desc = "Hover Documentation", unpack(bufopts) })
-  vim.keymap.set('n', '<leader>l<C-k>', vim.lsp.buf.signature_help, { desc = "Signature Help", unpack(bufopts) })
-  vim.keymap.set('n', '<leader>lr', vim.lsp.buf.rename, { desc = "Rename Symbol", unpack(bufopts) })
-  vim.keymap.set('n', '<leader>lc', vim.lsp.buf.code_action, { desc = "Code Action", unpack(bufopts) })
-  vim.keymap.set('n', '<leader>li', vim.lsp.buf.references, { desc = "List References", unpack(bufopts) })
-  vim.keymap.set('n', '<leader>ld', vim.diagnostic.open_float, { desc = "Show Line Diagnostics", unpack(bufopts) })
-  vim.keymap.set('n', '<leader>l[d', vim.diagnostic.goto_prev, { desc = "Previous Diagnostic", unpack(bufopts) })
-  vim.keymap.set('n', '<leader>l]d', vim.diagnostic.goto_next, { desc = "Next Diagnostic", unpack(bufopts) })
-  vim.keymap.set('n', '<leader>lq', vim.diagnostic.setloclist, { desc = "Show Diagnostics List", unpack(bufopts) })
-  vim.keymap.set('n', '<leader>lf', function() vim.lsp.buf.format { async = true } end, { desc = "Format Buffer", unpack(bufopts) })
+	vim.keymap.set('n', '<leader>lgd', vim.lsp.buf.definition, { desc = "Go to Definition", unpack(bufopts) })
+	vim.keymap.set('n', '<leader>lgD', vim.lsp.buf.declaration, { desc = "Go to Declaration", unpack(bufopts) })
+	vim.keymap.set('n', '<leader>lgi', vim.lsp.buf.implementation, { desc = "Go to Implementation", unpack(bufopts) })
+	vim.keymap.set('n', '<leader>lgt', vim.lsp.buf.type_definition, { desc = "Go to Type Definition", unpack(bufopts) })
+	vim.keymap.set('n', '<leader>lK', vim.lsp.buf.hover, { desc = "Hover Documentation", unpack(bufopts) })
+	vim.keymap.set('n', '<leader>l<C-k>', vim.lsp.buf.signature_help, { desc = "Signature Help", unpack(bufopts) })
+	vim.keymap.set('n', '<leader>lr', vim.lsp.buf.rename, { desc = "Rename Symbol", unpack(bufopts) })
+	vim.keymap.set('n', '<leader>lc', vim.lsp.buf.code_action, { desc = "Code Action", unpack(bufopts) })
+	vim.keymap.set('n', '<leader>li', vim.lsp.buf.references, { desc = "List References", unpack(bufopts) })
+	vim.keymap.set('n', '<leader>ld', vim.diagnostic.open_float, { desc = "Show Line Diagnostics", unpack(bufopts) })
+	vim.keymap.set('n', '<leader>l[d', vim.diagnostic.goto_prev, { desc = "Previous Diagnostic", unpack(bufopts) })
+	vim.keymap.set('n', '<leader>l]d', vim.diagnostic.goto_next, { desc = "Next Diagnostic", unpack(bufopts) })
+	vim.keymap.set('n', '<leader>lq', vim.diagnostic.setloclist, { desc = "Show Diagnostics List", unpack(bufopts) })
+	vim.keymap.set('n', '<leader>lf', function() vim.lsp.buf.format { async = true } end,
+		{ desc = "Format Buffer", unpack(bufopts) })
 end
 
 -- Auto-setup all available servers using Mason's setup_handlers
 require('mason-lspconfig').setup_handlers({
-    -- Default setup for all servers
-    function(server_name)
-        require('lspconfig')[server_name].setup {
-            on_attach = on_attach,
-        }
-    end,
+	-- Default setup for all servers
+	function(server_name)
+		require('lspconfig')[server_name].setup {
+			on_attach = on_attach,
+		}
+	end,
 })
 
 -- barbar
-vim.keymap.set("n", "<leader>bn", ":BufferNext<CR>", { desc = "Buffer Next" , noremap = true, silent = true })
-vim.keymap.set("n", "<leader>bp", ":BufferPrevious<CR>", { desc = "Buffer Next" , noremap = true, silent = true })
+vim.keymap.set("n", "<leader>bn", ":BufferNext<CR>", { desc = "Buffer Next", noremap = true, silent = true })
+vim.keymap.set("n", "<leader>bp", ":BufferPrevious<CR>", { desc = "Buffer Next", noremap = true, silent = true })
+vim.keymap.set("n", "<leader>bc", ":BufferClose<CR>", { desc = "Buffer Next", noremap = true, silent = true })
 
 -- splitting windows
-vim.api.nvim_set_keymap('n', '<leader>sv', ':vsplit<CR>', { noremap = true, silent = true })  -- Vertical split
-vim.api.nvim_set_keymap('n', '<leader>sh', ':split<CR>', { noremap = true, silent = true })   -- Horizontal split
+vim.api.nvim_set_keymap('n', '<leader>sv', ':vsplit<CR>', { noremap = true, silent = true }) -- Vertical split
+vim.api.nvim_set_keymap('n', '<leader>sh', ':split<CR>', { noremap = true, silent = true })  -- Horizontal split
